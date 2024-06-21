072cb069fc779aae40d28645a744eaf2e87934eb
dannyhollis <dannyhollisdevelopment@gmail.com>
2024-06-21 09:54:22 -0400

    one more line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div>This is just a git test</div>
+      <div>This is some more stuff to be pushed into the world</div>
     </main>
   );
 }
